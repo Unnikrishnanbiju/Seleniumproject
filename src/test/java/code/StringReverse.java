ff60842f8ee47b115f5f58376e78e564b2a9827a
Biju <unni1995biju@gmail.com>
2025-08-18 19:20:14 +0530

    3nd commit

---
+++ src/test/java/code/StringReverse.java
@@ -0,0 +1,8 @@
+package code;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        System.out.println(new StringBuilder(s1).reverse().toString());
+    }
+}
