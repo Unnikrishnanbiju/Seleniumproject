46f8f53424041c2c4e88ae3644dcd2587ad56a37
Biju <unni1995biju@gmail.com>
2025-08-20 11:13:35 +0530

    4nd commit

--- src/test/java/code/Palindrome.java
+++ src/test/java/code/Palindrome.java
@@ -9,7 +9,7 @@ public class Palindrome {
              System.out.println("Palindrome");
          }
          else {
-             System.out.println("Not  a Palindrome");
+             System.out.println("Not   the a Palindrome");
          }
     }
 }
