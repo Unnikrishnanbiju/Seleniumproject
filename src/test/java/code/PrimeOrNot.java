89e45a2e076f9ff29a39b3b3ee18d53748e57017
Biju <unni1995biju@gmail.com>
2025-08-21 12:33:43 +0530

    4nd commit

--- src/test/java/code/PrimeOrNot.java
+++ src/test/java/code/PrimeOrNot.java
@@ -4,7 +4,7 @@ public class PrimeOrNot {
     public static void main(String[] args) {
         int num = 8;
         boolean isPrime = num > 1;
-        for (int i = 2; i <num/2 ; i++) {
+        for (int i = 2; i <= Math.sqrt(num) ; i++) {
             if (num % i == 0){
                 isPrime = false;
                 break;
