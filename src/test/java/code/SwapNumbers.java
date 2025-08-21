250ace1f532216408bc9a7f5f55110cd24f54103
Biju <unni1995biju@gmail.com>
2025-08-21 13:03:07 +0530

    5nd commit

---
+++ src/test/java/code/SwapNumbers.java
@@ -0,0 +1,12 @@
+package code;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println(a+ " "+b );
+    }
+}
