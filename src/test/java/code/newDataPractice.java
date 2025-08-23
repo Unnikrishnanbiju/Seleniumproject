20e84c4b282fd7742db5da74373c5a51f72188a4
Biju <unni1995biju@gmail.com>
2025-08-23 15:48:02 +0530

    5nd commit

--- src/test/java/code/newDataPractice.java
+++ src/test/java/code/newDataPractice.java
@@ -1,10 +1,21 @@
 package code;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class newDataPractice {
     public static void main(String[] args) {
         System.out.println("********************************************");
         System.out.println("COUNT Character Frequency");
         System.out.println("********************************************");
+
+
+        String s = "automation";
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c :s.toCharArray())
+            map.put(c, map.getOrDefault(c,0)+1);
+
+        System.out.println(map);
 
 
 
