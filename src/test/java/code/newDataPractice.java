ab15ce71041d0c280bd80329abe1a488d05a7028
Biju <unni1995biju@gmail.com>
2025-08-23 15:23:32 +0530

    5nd commit

---
+++ src/test/java/code/newDataPractice.java
@@ -0,0 +1,12 @@
+package code;
+
+public class newDataPractice {
+    public static void main(String[] args) {
+        System.out.println("********************************************");
+        System.out.println("COUNT Character Frequency");
+        System.out.println("********************************************");
+
+
+
+    }
+}
