8c75e46e962f3bc8015ab7d93044341d3e9a2c85
Biju <unni1995biju@gmail.com>
2025-08-26 10:36:51 +0530

    5nd commit

---
+++ src/test/java/code/SecondSmallestElement.java
@@ -0,0 +1,10 @@
+package code;
+import java.util.*;
+
+public class SecondSmallestElement {
+    public static void main(String[] args) {
+        int[] arr= {5,4,3,6,1,};
+        Arrays.sort(arr);
+        System.out.println("Second Smallest"+" " +arr[1]);
+    }
+}
