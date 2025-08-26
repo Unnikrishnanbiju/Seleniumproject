dfd031287157754fc7e6a34170cff05008a8de5f
Biju <unni1995biju@gmail.com>
2025-08-26 10:38:21 +0530

    5nd commit

--- src/test/java/code/SecondSmallestElement.java
+++ src/test/java/code/SecondSmallestElement.java
@@ -6,5 +6,6 @@ public class SecondSmallestElement {
         int[] arr= {5,4,3,6,1,};
         Arrays.sort(arr);
         System.out.println("Second Smallest"+" " +arr[1]);
+        System.out.println("Second Largest"+ " " + arr[arr.length-2]);
     }
 }
