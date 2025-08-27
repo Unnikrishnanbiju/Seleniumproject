b840da374804f452dc6a036f20bea3f1c9c05ebc
Biju <unni1995biju@gmail.com>
2025-08-27 20:16:29 +0530

    5nd commit

---
+++ src/test/java/code/REverseStringNoReverse.java
@@ -0,0 +1,15 @@
+package code;
+
+public class REverseStringNoReverse {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String rev = " ";
+        for(int i = s1.length()-1; i >= 0; i++){
+            rev += s1.charAt(i);
+        }
+        System.out.println(rev);
+    }
+
+
+
+}
