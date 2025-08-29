695249603665f565554a7325a4ab795b7a89d49e
Biju <unni1995biju@gmail.com>
2025-08-29 22:20:41 +0530

    5nd commit

--- src/test/java/code/REmoveDuplicateString.java
+++ src/test/java/code/REmoveDuplicateString.java
@@ -1,15 +1,19 @@
 package code;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class REmoveDuplicateString {
     public static void main(String[] args) {
         int[] arr = {7, -5, 3, 8, -4, 11, -19, 21, 3, -5, 7, 8,1,1,1,1};
+
+        Arrays.sort(arr);
         Set <Integer> set = new LinkedHashSet<>();
         for (int num : arr){
             set.add(num);
         }
         System.out.println(set  );
+        System.out.println(Arrays.toString(arr));
     }
 }
