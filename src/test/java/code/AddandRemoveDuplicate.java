0154af618b867c9276ae0e798932a4e97fdacc84
Biju <unni1995biju@gmail.com>
2025-08-29 17:39:14 +0530

    5nd commit

--- src/test/java/code/AddandRemoveDuplicate.java
+++ src/test/java/code/AddandRemoveDuplicate.java
@@ -1,7 +1,24 @@
 package code;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class AddandRemoveDuplicate {
     public static void main(String[] args) {
+        int[] arr1 = {7, -5, 3, 8, -4, 11, -19, 21};
+        int[] arr2 = {6, 13, -7, 0, -4, 3, -5};
 
+        Set<Integer> resultSet = new LinkedHashSet<>();
+
+        for(int num :arr1){
+            resultSet.add(num);
+
+        }
+
+        for(int num :arr2){
+            resultSet.add(num);
+
+        }
+        Integer[] mergedArray = resultSet.toArray(new Integer[0]);
     }
 }
