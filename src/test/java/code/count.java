840b5f088b9db5a82f162e546b46ef90d6a408bb
Biju <unni1995biju@gmail.com>
2025-08-30 17:53:50 +0530

    5nd commit

---
+++ src/test/java/code/count.java
@@ -0,0 +1,20 @@
+package code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class count {
+    public static void main(String[] args) {
+        String str = "automation";
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char s : str.toCharArray()){
+            map.put(s, map.getOrDefault(s,0)+1);
+        }
+        System.out.println(map);
+    }
+
+
+}
