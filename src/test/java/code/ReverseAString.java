c9eb63b19d3f4fb2c9c4e1e600f3f6fb6278ab2d
Biju <unni1995biju@gmail.com>
2025-08-30 20:52:52 +0530

    5nd commit

--- src/test/java/code/ReverseAString.java
+++ src/test/java/code/ReverseAString.java
@@ -2,13 +2,20 @@ package code;
 
 public class ReverseAString {
     public static void main(String[] args) {
-        String name = "Unni krishnan";
+        String name = "maam";
         System.out.println(new StringBuilder(name).reverse().toString());
 
-        String rev = " ";
+        String rev ="";
+
         for (int i = name.length()-1; i >=0 ; i--) {
             rev = rev + name.charAt(i);
 
+        }
+        if (name.equals(rev)){
+            System.out.println("palindrome");
+
+        }else {
+            System.out.println("Not");
         }
         System.out.println(rev+"  2nd one");
 
