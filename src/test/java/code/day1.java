37f4d06d7d4cb837b4fd237837938f4e700bc8e0
Biju <unni1995biju@gmail.com>
2025-09-01 15:59:39 +0530

    day1

--- src/test/java/code/day1.java
+++ src/test/java/code/day1.java
@@ -4,10 +4,22 @@ import java.sql.SQLOutput;
 
 public class day1 {
     public static void main(String[] args) {
-        String arr = "javascript";
+        String arr = "maam";
 
         System.out.println("*********************************************");
         System.out.println(new StringBuilder(arr).reverse().toString());
+        System.out.println("*********************************************");
+        String rev = new StringBuilder(arr).reverse().toString();
+        if (arr.equals(rev)){
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not");
+        }
+        System.out.println("*********************************************");
+        System.out.println("*********************************************");
+        System.out.println("*********************************************");
+        System.out.println("*********************************************");
         System.out.println("*********************************************");
     }
 }
