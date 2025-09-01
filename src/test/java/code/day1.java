25754bbe06dee3ac77e42e09d75b9a8000b10115
Biju <unni1995biju@gmail.com>
2025-09-01 15:54:26 +0530

    day1

---
+++ src/test/java/code/day1.java
@@ -0,0 +1,13 @@
+package code;
+
+import java.sql.SQLOutput;
+
+public class day1 {
+    public static void main(String[] args) {
+        String arr = "javascript";
+
+        System.out.println("*********************************************");
+        System.out.println(new StringBuilder(arr).reverse().toString());
+        System.out.println("*********************************************");
+    }
+}
