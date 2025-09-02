d73bcff6af6677fef93ec5bef4bbc3622ca9edb9
Biju <unni1995biju@gmail.com>
2025-09-02 16:15:24 +0530

    day1

---
+++ src/test/java/code/REmoveDuplicate.java
@@ -0,0 +1,12 @@
+package code;
+
+public class REmoveDuplicate {
+    public static void main(String[] args) {
+        String str = "programmingg", res ="";
+        for (char c : str.toCharArray())
+            if (res.indexOf(c)<0)res += c;
+        System.out.println(res);
+
+    }
+}
+
--- src/test/java/code/day1.java
+++ src/test/java/code/day1.java
@@ -99,6 +99,19 @@ char ch = 'l';
         System.out.println(map);
 
         System.out.println("*********************************************");
+        String str3 = "12345";
+        int num = 0;
+
+        for (int i = 0; i < str3.length(); i++) {
+            char c = str3.charAt(i);
+
+            // Convert char to digit by subtracting '0'
+            int digit = c - '0';
+            num = num * 10 + digit;
+        }
+
+        System.out.println("Converted number: " + num);
+
 
         System.out.println("*********************************************");
 
