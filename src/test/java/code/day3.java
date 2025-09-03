edac755710a664e836ba9372e0e1749c821434a1
Biju <unni1995biju@gmail.com>
2025-09-03 10:49:26 +0530

    day3

---
+++ src/test/java/code/day3.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.util.Arrays;
+
+public class day3 {
+    public static void main(String[] args) {
+        int[] a = {2,3,5,6,3,1,5};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        System.out.println("************************************************************");
+
+        System.out.println("************************************************************");
+        System.out.println("************************************************************");
+        System.out.println("************************************************************");
+        System.out.println("************************************************************");
+        System.out.println("************************************************************");
+    }
+}
