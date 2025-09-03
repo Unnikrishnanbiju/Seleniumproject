1d63e33b657d761c7ff15f6a9e21331cfb93a041
Biju <unni1995biju@gmail.com>
2025-09-03 10:54:46 +0530

    day3

--- src/test/java/code/day3.java
+++ src/test/java/code/day3.java
@@ -7,8 +7,16 @@ public class day3 {
         int[] a = {2,3,5,6,3,1,5};
         Arrays.sort(a);
         System.out.println(Arrays.toString(a));
-        System.out.println("************************************************************");
-
+        System.out.println("***********************MIN and MAX*************************************");
+int [] arr = {10,20,30,22,44,73};
+int min = arr[0];
+int max = arr[0];
+for (int num :arr){
+    if (num < min)min = num;
+    if (num > max)max = num;
+}
+        System.out.println(min);
+        System.out.println(max);
         System.out.println("************************************************************");
         System.out.println("************************************************************");
         System.out.println("************************************************************");
