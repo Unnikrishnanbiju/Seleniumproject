68a0add89c4b67e79a53399e87dcc23ac1f4232c
Biju <unni1995biju@gmail.com>
2025-09-07 20:40:44 +0530

    day4

--- src/test/java/Array/Day3.java
+++ src/test/java/Array/Day3.java
@@ -5,7 +5,7 @@ public class Day3 {
         System.out.println("***************************Reverse a String****************************************");
         String str = "Hello";
         System.out.println(new StringBuilder(str).reverse().toString());
-        System.out.println("***************************Plindrome****************************************");
+        System.out.println("***************************Palindrome****************************************");
         String str1 = "maam";
         String rev = new StringBuilder(str1).reverse().toString();
         if (str1.equals(rev)){
@@ -13,7 +13,15 @@ public class Day3 {
         }else {
             System.out.println("NOT");
         }
-        System.out.println("*******************************************************************");
+        System.out.println("******************************First non repeated character in String*************************************");
+        String str3 = "success";
+        for(char c : str3.toCharArray()){
+            if (str3.indexOf(c) == str3.lastIndexOf(c)){
+                System.out.println("First non-reapeated number is :"+c);
+                return;
+            }
+        }
+        System.out.println("No non-repeated number");
         System.out.println("*******************************************************************");
         System.out.println("*******************************************************************");
         System.out.println("*******************************************************************");
