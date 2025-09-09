f2ff743752a054ed1c6ed64095807e426f0461a3
Biju <unni1995biju@gmail.com>
2025-09-09 09:28:08 +0530

    day4

---
+++ src/test/java/Array/RevrseStringWithOutReverse.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class RevrseStringWithOutReverse {
+    public static void main(String[] args) {
+        String str = "hello";
+        String rev = "";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            rev += str.charAt(i);
+
+        }
+        System.out.println(rev);
+    }
+}
