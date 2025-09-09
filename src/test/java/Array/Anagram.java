c2e734ee7f9435685ee33cdc5527c2c5073d046d
Biju <unni1995biju@gmail.com>
2025-09-09 09:32:04 +0530

    day4

--- src/test/java/Array/Anagram.java
+++ src/test/java/Array/Anagram.java
@@ -2,6 +2,16 @@ package Array;
 
 public class Anagram {
     public static void main(String[] args) {
+String str1 = "Listen";
+String str2 = "Silent";
+
+str1 = str1.replaceAll("\\s+","").toLowerCase();
+        str2 = str2.replaceAll("\\s+","").toLowerCase();
+
+        if (str1.length() ! = str2.length()){
+            System.out.println("NOt anagram");
+            return;
+        }
 
     }
 }
