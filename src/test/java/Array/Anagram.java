3d8e2c4ec93cef42a61ab77f70531442b5cbfaee
Biju <unni1995biju@gmail.com>
2025-09-09 09:36:04 +0530

    day4

--- src/test/java/Array/Anagram.java
+++ src/test/java/Array/Anagram.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.Arrays;
+
 public class Anagram {
     public static void main(String[] args) {
 String str1 = "Listen";
@@ -8,10 +10,21 @@ String str2 = "Silent";
 str1 = str1.replaceAll("\\s+","").toLowerCase();
         str2 = str2.replaceAll("\\s+","").toLowerCase();
 
-        if (str1.length() ! = str2.length()){
+        if (str1.length() != str2.length()){
             System.out.println("NOt anagram");
             return;
         }
+char[] arr1 = str1.toCharArray();
+        char[] arr2 = str1.toCharArray();
 
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1,arr2)){
+            System.out.println("Anagram");
+        }
+        else {
+            System.out.println("Not anagram");
+        }
     }
 }
